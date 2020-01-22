9d01096f69af709b72e5d2531edbe4801529962e
catepalmer <cate@springload.co.nz>
2020-01-22 14:47:22 +1300

    Add New Game button

--- src/components/BoardSizeSelect.tsx
+++ src/components/BoardSizeSelect.tsx
@@ -1,34 +1,33 @@
-import React from 'react';
-import {
-    useDispatch,
-    useSelector as useReduxSelector,
-    TypedUseSelectorHook,
-} from 'react-redux';
-import { AppState } from '../reducers/index';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { smallBoard, medBoard, largeBoard } from '../constants';
 import actions from '../actions/actions';
 import '../sass/styles.scss';
 
 const BoardSizeSelect = () => {
     const dispatch = useDispatch();
-    const useSelector: TypedUseSelectorHook<AppState> = useReduxSelector;
-    const boardSize = useSelector(state => state.board.boardSize);
-    const setBoardSize = (boardSize: string) => {
+    const [boardSize, setBoardSize] = useState('');
+    
+    const handleClick = () => {
         const boardSizes = [smallBoard, medBoard, largeBoard];
         const board = boardSizes.find(boardType => {
             return boardType.boardSize === boardSize;
         });
         if (board) dispatch(actions.setBoardSize(board));
     };
+
     return (
-        <form>
-        <select onChange={e => setBoardSize(e.target.value)} value={boardSize}>
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
-        </select>
-        <input type="submit" value="New Game"></input>
-        </form>
+        <>
+            <select
+                onChange={e => setBoardSize(e.target.value)}
+                value={boardSize}
+            >
+                <option value="small">Small</option>
+                <option value="medium">Medium</option>
+                <option value="large">Large</option>
+            </select>
+            <button onClick={handleClick}>New Game</button>
+        </>
     );
 };
 
