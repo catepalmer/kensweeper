5cbe4813ca6fe60ef4cb424593031b02d11aadd9
Cate Palmer <catherinepalmer@gmail.com>
2020-04-24 15:47:30 +1200

    Fix board not resetting when selecting small board

--- src/components/BoardSizeSelect.tsx
+++ src/components/BoardSizeSelect.tsx
@@ -1,34 +1,31 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { smallBoard, medBoard, largeBoard } from '../constants';
 import actions from '../actions/actions';
 import '../sass/styles.scss';
 
 const BoardSizeSelect = () => {
-    const dispatch = useDispatch();
-    const [boardSize, setBoardSize] = useState('');
+	const dispatch = useDispatch();
+	const [ boardSize, setBoardSize ] = useState('small');
 
-    const handleClick = () => {
-        const boardSizes = [smallBoard, medBoard, largeBoard];
-        const board = boardSizes.find(boardType => {
-            return boardType.boardSize === boardSize;
-        });
-        if (board) dispatch(actions.setBoardSize(board));
-    };
+	const handleClick = () => {
+		const boardSizes = [ smallBoard, medBoard, largeBoard ];
+		const board = boardSizes.find((boardType) => {
+			return boardType.boardSize === boardSize;
+		});
+		if (board) dispatch(actions.setBoardSize(board));
+	};
 
-    return (
-        <>
-            <select
-                onChange={e => setBoardSize(e.target.value)}
-                value={boardSize}
-            >
-                <option value="small">Small</option>
-                <option value="medium">Medium</option>
-                <option value="large">Large</option>
-            </select>
-            <button onClick={handleClick}>New Game</button>
-        </>
-    );
+	return (
+		<Fragment>
+			<select onChange={(e) => setBoardSize(e.target.value)} value={boardSize}>
+				<option value="small">Small</option>
+				<option value="medium">Medium</option>
+				<option value="large">Large</option>
+			</select>
+			<button onClick={handleClick}>New Game</button>
+		</Fragment>
+	);
 };
 
 export default BoardSizeSelect;
