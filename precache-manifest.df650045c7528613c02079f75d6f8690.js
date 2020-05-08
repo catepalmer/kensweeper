0e9ba39b45c155227bdcd46697298966ef5dd970
Cate Palmer <catherinepalmer@gmail.com>
2020-05-08 14:04:00 +1200

    Updates

---
+++ precache-manifest.df650045c7528613c02079f75d6f8690.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d35e0da327dfcb19cd2d4201e62a018",
+    "url": "/kensweeper/index.html"
+  },
+  {
+    "revision": "61a0cf5bc7a378dacd47",
+    "url": "/kensweeper/static/css/main.4bb0c89f.chunk.css"
+  },
+  {
+    "revision": "8b57187dad3fd1294d18",
+    "url": "/kensweeper/static/js/2.5c7b67d7.chunk.js"
+  },
+  {
+    "revision": "61a0cf5bc7a378dacd47",
+    "url": "/kensweeper/static/js/main.2ad6c5b8.chunk.js"
+  },
+  {
+    "revision": "98f85b8573b7c103338d",
+    "url": "/kensweeper/static/js/runtime-main.8e3f687c.js"
+  },
+  {
+    "revision": "41007bfff3c38dcb0798848ffd7be03d",
+    "url": "/kensweeper/static/media/flag.41007bff.png"
+  },
+  {
+    "revision": "47dc84886cb35bade4765957bd209a35",
+    "url": "/kensweeper/static/media/ken-sad.47dc8488.png"
+  },
+  {
+    "revision": "394aa9ea1ef23cbba00179c11a74c753",
+    "url": "/kensweeper/static/media/ken-sunglasses.394aa9ea.png"
+  },
+  {
+    "revision": "9703d12e9bd58dc19f3f644c03eb47f2",
+    "url": "/kensweeper/static/media/ken.9703d12e.jpg"
+  },
+  {
+    "revision": "6a757fd39d070962c3bb3cc568e37b49",
+    "url": "/kensweeper/static/media/mine.6a757fd3.png"
+  }
+]);
